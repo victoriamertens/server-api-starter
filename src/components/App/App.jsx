0f7e260b6f49701cd3d8cdb4bf6927be605613f4
Victoria Mertens <111324946+victoriamertens@users.noreply.github.com>
2022-12-06 09:50:48 -0600

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App () {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">APIS</h1>
+          <h4><i>APIS</i></h4>
+        </header>
+      </div>
+    );
+  
+}
+
+export default App;
